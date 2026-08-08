c814aade4186156988f72b7df623b620a7340a72
sreevallisarvasuddi <sreevalli0212@gmail.com>
2026-08-08 11:30:02 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -1,1 +1,19 @@
+# output
+# print(value,...,sep='',end='\n',flush=False)
+print('Hello World')
+print(7)
+print(True)
+print('Hello',1,2.5,True)
 
+# data types
+print(7) # integer
+print(1e309) # python can't handle and gives inf
+print(1.7e308)
+print(8.55) # floating numbers
+print(True) # boolean
+print('This is a string') # string
+print(5+6j) # complex number
+print([1,2,3,4,5]) # list
+print((1,2,3,4,5)) # tuple
+print({1,2,3,4,5}) # set
+print({'Name':'Siri','Age':19,'Course':'BTech'}) # dictionary
