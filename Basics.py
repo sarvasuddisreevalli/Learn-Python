3b0577f10bb84d9b14c40ce8bc0dc506d4b78f77
sreevallisarvasuddi <sreevalli0212@gmail.com>
2026-08-08 12:05:52 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -1,11 +1,11 @@
-# output
+# output :
 # print(value,...,sep='',end='\n',flush=False)
 print('Hello World')
 print(7)
 print(True)
 print('Hello',1,2.5,True)
 
-# data types
+# data types :
 print(7) # integer
 print(1e309) # python can't handle and gives inf
 print(1.7e308)
@@ -19,7 +19,7 @@ print({1,2,3,4,5}) # set
 print({'Name':'Siri','Age':19,'Course':'BTech'}) # dictionary
 type({2}) # func that detects data type
 
-# variables
+# variables :
 name = 'sreevalli'
 a = 10
 b = 9
@@ -40,4 +40,46 @@ print(y)
 a,b,c = 1,2,3
 a=b=c = 5
 
+# comments :
+# written for developer comfort & not executable part
+# No multiple line commands in python
+# first comment
+a = 1 # integer
+
+# keywords :
+# False
+# None
+# True
+# and
+# as
+# assert
+# break
+# class
+# continue
+# def
+# del
+# elif
+# else
+# except
+# finally
+# for
+# from
+# global
+# if
+# import
+# in
+# is
+# lambda
+# nonlocal
+# not
+# or
+# pass
+# raise
+# return
+# try
+# while
+# with
+# yield
+
 #
+
