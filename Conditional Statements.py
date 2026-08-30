fbff37a8b6c8d74e2f5a596398b8c6ccda8e6401
sreevallisarvasuddi <sreevalli0212@gmail.com>
2026-08-30 10:07:59 +0530

    Update Conditional Statements.py

--- Conditional Statements.py
+++ Conditional Statements.py
@@ -30,6 +30,19 @@ friends //= 3 #(interger division)
 remaining_friends = friends % 2
 friends %= 2
 
+# logical operators
+# (and, or, not)
+temp = 30
+sunny = False
+if temp<0 or temp>35:
+    print('Outdoor event is cancelled')
+elif temp>25 and sunny:
+    print('It is sunny')
+elif not sunny:
+    print("It's cloudy")
+else:
+    print('Outdoor event is still scheduled')
+
 # Type casting
 # User input is always a string so type conversion needed
 name = 'siri'
@@ -42,3 +55,17 @@ subjects = float(subjects)
 subjects = str(subjects)
 name = bool(name)
 print(type(name))
+
+# while loop
+while False:
+    print('Infinite loop')
+username = input("Enter your name\n")
+
+while username=="":
+    username = input("Enter your name\n")
+#takes input until user enters their name
+print(f"Hello {username} !")
+
+
+
+
