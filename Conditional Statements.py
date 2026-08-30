b602e8f773f90db53a8169233e3cad38cbb13675
sreevallisarvasuddi <sreevalli0212@gmail.com>
2026-08-30 09:47:31 +0530

    Update Conditional Statements.py

--- Conditional Statements.py
+++ Conditional Statements.py
@@ -1,9 +1,22 @@
 # if-else statement
+age = int(input("Enter your age:\n"))
+if age >= 18 and age <65:
+  print("You're an adult")
+elif age >= 65:
+    print("You're a senior citizen")
+elif age < 0:
+    print("Invalid age")
+elif age == 0:
+    print("You're just born")
+else:
+  print("You're a child")
+  
+# if-else boolean example
 is_student = True
 if is_student:
-  print("You're a student")
+    print("You're a student")
 else:
-  print("You're not a student")
+    print("You're not a student")
 
 # arithmetic operators
 # (=,+,-,*,/,//,%)
@@ -20,12 +33,12 @@ friends %= 2
 # Type casting
 # User input is always a string so type conversion needed
 name = 'siri'
-age = 19
+subjects = 19
 gpa = 8.99
 is_prime = True
 # type(gpa) --> to check datatype
 gpa = int(gpa)
-age = float(age)
-age = str(age)
+subjects = float(subjects)
+subjects = str(subjects)
 name = bool(name)
 print(type(name))
